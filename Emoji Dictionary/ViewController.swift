6fd820f92c57de53dae4ec7b58d8e8cba19ad248
Jack O'Reilly <jackoreilly@Jacks-MBP.gateway>
2017-07-09 11:13:07 +1000

    Emojis into the tableview

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableview: UITableView!
     
+    var emojis = ["🤓","🤑","😍","🤠","😎","😇"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +22,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
         let cell = UITableViewCell()
-        cell.textLabel?.text = "🤓"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
